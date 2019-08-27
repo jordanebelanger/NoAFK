ef721844b004577c258b610201c873300a0cfb73
Jordane Belanger <jordane.belanger@gmail.com>
2019-08-27 15:03:56 -0400

    Remove dead code

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 // Type "Hello World" then press enter.
 var robot = require("robotjs");
 
-// Type "Hello World".
-
-async function loop(runFunc) {
-    await runFunc()
-}
-
 setInterval(() => {
     robot.keyTap("w");    
     robot.keyTap("a");    
